b32aa6367ece661db4fa533873a18f72d070500d
MD RAJ <rajmd1800@gmail.com>
2025-09-04 13:55:37 +0530

    email feature added but not working

--- BackEnd/Week5/2.FileUpload/models/File.js
+++ BackEnd/Week5/2.FileUpload/models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -17,5 +18,35 @@ const fileSchema = new mongoose.Schema({
     }
 });
 
+//post middleware
+// db me entry create karne k turant bad jo kam karna hai uske liye post middleware use hota hai aur entry se just phle jo kam karna hota hai uske liye pre middleware use hota hai
+
+fileSchema.post("save",async function(doc) {
+    try{
+        console.log(doc)
+        //db me jo bhi entry create ho raha hai wahi hai doc
+
+        //transporter
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS,
+            },
+        });
+
+        //send mail
+        let info = await transporter.sendMail({
+            from:`mdraj test purpose`,
+            to:doc.email,
+            subject:"new file uploaded on cloudinary",
+            html:`<h2>Namaskaram</h2><p>File uploaded successfully View here:<a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+        })
+        console.log("Info",info);
+    }
+    catch(error){
+        console.log(error);
+    }
+})
 const File = mongoose.model("File",fileSchema);
 module.exports = File;
